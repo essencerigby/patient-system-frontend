117b329ef329b1a19f9d476281e9c81faea77877
Shaddy Abdou <sabdou@catalyte.io>
2024-06-10 10:54:25 -0400

    added getProducts to apiService file

--- src/apiService.jsx
+++ src/apiService.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const vendorsApiUrl = 'http://localhost:8085/vendors';
+const productsApiUrl = 'http://localhost:8085/products';
 
 export const getVendors = async () => {
   try {
@@ -17,5 +18,23 @@ export const getVendorById = async (id) => {
     return response.data;
   } catch (error) {
     throw Error('Could not fetch vendor', error);
+  }
+};
+
+export const getProducts = async () => {
+  try {
+    const response = await axios.get(productsApiUrl);
+    return response.data;
+  } catch (error) {
+    throw Error('Could not fetch products', error);
+  }
+};
+
+export const getProductById = async (id) => {
+  try {
+    const response = await axios.get(`${productsApiUrl}/${id}`);
+    return response.data;
+  } catch (error) {
+    throw Error('Could not fetch product', error);
   }
 };
