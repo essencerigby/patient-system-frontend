1793e497b3b92a1ead1ffb968e78cdc5f174ef33
Kelsey Belanger <kbelanger@catalyte.io>
2024-06-12 14:57:10 +0000

    RUNNERS-65-UI-Header-Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductPage from './Product/ProductPage';
+import VendorPage from './Vendor/VendorPage';
+import ResponsiveAppBar from './Component/Header';
 
 /**
  * App component serves as the main entry point for the application.
  *
  * @component
- * @returns {JSX.Element} A React component representing the main application.
+ * @returns {JSX.Element} A React component representing the main
+ * application with links to Vendor, Customer, Product, and Promotions pages.
  */
 function App() {
   return (
     <div className='App'>
-      <ProductPage />
+      <Router>
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path='/vendors' element={<VendorPage />} />
+          {/* <Route path='/customers' element={<Customers />} /> */}
+          <Route path='/products' element={<ProductPage />} />
+          {/* <Route path='/promotions' element={<Promotions />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
