1940036836f14f0b5f2e560297608ce2399986be
Shaddy Abdou <sabdou@catalyte.io>
2024-06-15 07:37:36 -0400

    changed keys for vendor, refactored handleSubmit

--- src/Vendor/AddVendor.jsx
+++ src/Vendor/AddVendor.jsx
@@ -6,35 +6,32 @@ import VendorForm from './VendorForm';
 // Array of fields for the form
 const fields = [
   { id: 'name', label: 'Name', keys: 'name' },
-  { id: 'address.street', label: 'Street', keys: 'street' },
-  { id: 'address.street2', label: 'Street 2', keys: 'street2' },
-  { id: 'address.city', label: 'City', keys: 'city' },
-  { id: 'address.state', label: 'State', keys: 'state' },
-  { id: 'address.zipCode', label: 'Zip Code', keys: 'zipCode' },
-  { id: 'contact.email', label: 'Email', keys: 'email' },
-  { id: 'contact.contactName', label: 'Contact Name', keys: 'contactName' },
-  { id: 'contact.titleOrRole', label: 'Title or Role', keys: 'titleOrRole' },
-  { id: 'contact.phone', label: 'Phone', keys: 'phone' }
+  { id: 'street', label: 'Street', keys: 'street' },
+  { id: 'street2', label: 'Street 2', keys: 'street2' },
+  { id: 'city', label: 'City', keys: 'city' },
+  { id: 'state', label: 'State', keys: 'state' },
+  { id: 'zipCode', label: 'Zip Code', keys: 'zipCode' },
+  { id: 'email', label: 'Email', keys: 'email' },
+  { id: 'contactName', label: 'Contact Name', keys: 'contactName' },
+  { id: 'titleOrRole', label: 'Title or Role', keys: 'titleOrRole' },
+  { id: 'phone', label: 'Phone', keys: 'phone' }
 ];
 
 export default function AddVendor({ onRefresh }) {
   const [modal, setModal] = useState(false);
   const [modalWidth, setModalWidth] = useState(Math.min(window.innerWidth * 0.8, 600));
   const [vendor, setVendor] = useState({
+    id: '',
     name: '',
-    address: {
-      street: '',
-      street2: '',
-      city: '',
-      state: '',
-      zipCode: ''
-    },
-    contact: {
-      email: '',
-      contactName: '',
-      titleOrRole: '',
-      phone: ''
-    }
+    street: '',
+    street2: '',
+    city: '',
+    state: '',
+    zipCode: '',
+    email: '',
+    contactName: '',
+    titleOrRole: '',
+    phone: ''
   });
 
   const [error, setError] = useState(null);
@@ -61,46 +58,48 @@ export default function AddVendor({ onRefresh }) {
   // This function handles changes in input fields of the vendor form.
   const handleChange = (e) => {
     const { id, value } = e.target;
-    // Split the id into an array of keys to navigate the nested structure
-    const keys = id.split('.');
-    setVendor((prevValues) => {
-      const newVendor = { ...prevValues };
-      let current = newVendor;
-      // Traverse the nested structure until the second-to-last key.
-      // If the current key does not exist in the nested structure, create it
-      // Move the pointer to the next nested object
-      for (let i = 0; i < keys.length - 1; i += 1) {
-        if (!current[keys[i]]) current[keys[i]] = {};
-        current = current[keys[i]];
-      }
-      // Set the value of the last key in the nested structure to the input value
-      current[keys[keys.length - 1]] = value;
-      return newVendor; // Return the updated vendor object
-    });
+    setVendor((prevValues) => ({
+      ...prevValues,
+      [id]: value
+    }));
   };
 
   // This function handles the submission of the vendor form.
   const handleSubmit = async () => {
     try {
-      await createVendor(vendor);
+      const vendorToCreate = {
+        id: vendor.id,
+        name: vendor.name,
+        address: {
+          street: vendor.street,
+          street2: vendor.street2,
+          city: vendor.city,
+          state: vendor.state,
+          zipCode: vendor.zipCode
+        },
+        contact: {
+          contactName: vendor.contactName,
+          email: vendor.email,
+          titleOrRole: vendor.titleOrRole,
+          phone: vendor.phone
+        }
+      };
+      await createVendor(vendorToCreate);
       setError(null); // Reset error on successful submission
       toggleModal(); // Close the modal after successful submission
       onRefresh(); // Refresh the vendor list after successful submission
       setVendor({
+        id: '',
         name: '',
-        address: {
-          street: '',
-          street2: '',
-          city: '',
-          state: '',
-          zipCode: ''
-        },
-        contact: {
-          email: '',
-          contactName: '',
-          titleOrRole: '',
-          phone: ''
-        }
+        street: '',
+        street2: '',
+        city: '',
+        state: '',
+        zipCode: '',
+        email: '',
+        contactName: '',
+        titleOrRole: '',
+        phone: ''
       }); // Resetting the vendor state to its initial empty values after successful submission
     } catch (err) {
       setError(err.response ? err.response.data : err.message); // Set error if submission fails
