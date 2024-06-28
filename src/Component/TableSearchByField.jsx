9028fd135422904801ad55746b3127d76e533121
William Kirk <wkirk@catalyte.io>
2024-06-28 16:39:59 -0500

    Added dynamic dropdown list

--- src/Component/TableSearchByField.jsx
+++ src/Component/TableSearchByField.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 
 export default function TableSearchByField(props) {
   const [searchValue, setSearchValue] = useState('');
-  const { domainToSearch, setDomain, onRefresh } = props;
+  const [fieldToFilterBy, setFieldToFilterBy] = useState();
+  const {
+    domainToSearch, setDomain, onRefresh, fieldsToSearch
+  } = props;
 
   const handleWordInputChange = (event) => {
     const { value } = event.target;
     setSearchValue(value);
+    // onRefresh();
     console.log(value); // Log the updated value
+    console.log(fieldsToSearch);
   };
 
   const filterDomainByAttribute = (nameValue) => {
@@ -27,8 +33,17 @@ export default function TableSearchByField(props) {
         id='test'
         type='text'
         onChange={(event) => handleWordInputChange(event)}
-        style={{ position: 'relative', width: 200 }}
+        style={{ position: 'relative', width: 200, transform: 'translateX(-5%)' }}
       />
+
+      <select onChange={(event) => setFieldToFilterBy(event.target.value)}>
+        {fieldsToSearch.map((field) => (
+          <option key={field.id} value={field.id}>
+            {field.label}
+          </option>
+        ))}
+      </select>
+
       <button type='button' style={{ transform: 'translateX(10%)' }} className='btn-modal' onClick={() => { filterDomainByAttribute(searchValue); }}>
         <strong>Search</strong>
       </button>
